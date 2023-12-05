02dbd05c91e40eebc69823d57dd27fed5e645c6b
nzangi <nzangimuoki284@gmail.com>
2023-12-05 19:50:49 +0300

    first commit

--- app/App.php
+++ app/App.php
@@ -1,3 +1,43 @@
 <?php
     declare(strict_types = 1);
+
+    function getTransactionFiles(string $dirPath): array
+
+    {
+        $files = [];
+        foreach (scandir($dirPath) as $file){
+            if(is_dir($file)){
+                continue;
+            }
+            $files[] = $dirPath . $file;
+        }
+        return $files;
+    }
+    function getTransactions(string $fileName): array
+    {
+        if(! file_exists($fileName)){
+            trigger_error('File "' .$fileName . '" doesnot exist.',E_USER_ERROR);
+        }
+        $file = fopen($fileName,'r');
+        fgetcsv($file);
+        $transactions = [];
+
+        while (($transaction = fgetcsv($file)) != false){
+            $transactions[] = extractTransactions($transaction);
+        }
+        return $transactions;
+    }
+
+    function extractTransactions(array $transactionRow): array
+    {
+        [$date,$checkNumber,$description,$amount] = $transactionRow;
+        $amount = (float) str_replace(['$',','],'',$amount);
+
+        return [
+            'date' =>$date,
+            'checkNumber' =>$checkNumber,
+            'description' => $description,
+            'amount' => $amount,
+        ];
+    }
 ?>
--- public/index.php
+++ public/index.php
@@ -5,4 +5,13 @@
     define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
     define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
     define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+
+    require APP_PATH ."App.php";
+    $files = getTransactionFiles(FILES_PATH);
+    $transactions = [];
+    foreach ($files as $file){
+        $transactions = array_merge($transactions,getTransactions($file));
+    }
+
+    require VIEWS_PATH . 'transactions.php';
 ?>
